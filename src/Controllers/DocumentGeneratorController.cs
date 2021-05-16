a2ec08edb4b9d07d554fb76d9ab50ddcd77154b4
GreenArrowVigilante <GreenArrowVigilante>
2021-05-16 11:48:07 +0530

    initial commit

---
+++ src/Controllers/DocumentGeneratorController.cs
@@ -0,0 +1,35 @@
+﻿using HtmlToPdf.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentGeneratorController : ControllerBase
+    {
+        private readonly IDocumentService _documentService;
+
+        public DocumentGeneratorController(IDocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var pdfFile = _documentService.GeneratePdfFromString();
+            return File(pdfFile, "application/octet-stream", "SimplePdf.pdf");
+        }
+
+        [HttpGet("GetPdfFromRazor")]
+        public IActionResult GetPdfFromRazor()
+        {
+            var pdfFile = _documentService.GeneratePdfFromRazorView("/Views/PdfTemplate/InvoiceDetails.cshtml");
+            return File(pdfFile, "application/octet-stream", "RazorPdf.pdf");
+        }
+    }
+}
---
+++ src/Interfaces/IRazorRendererHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf.Interfaces
+{
+    public interface IRazorRendererHelper
+    {
+        string RenderPartialToString<TModel>(string partialName, TModel model);
+    }
+}
